d9121cc6302d04d3f27b095328b577e62279ef55
wildmove <muhammad.wildan,ft21@mail.umy.ac.id>
2022-11-25 16:17:25 +0700

    buat method firstNode

--- Exercise3_090/Program.cs
+++ Exercise3_090/Program.cs
@@ -64,6 +64,14 @@ namespace Exercise_Linked_List_A
                 Console.Write(LAST.rollNumber + " " + LAST.name + "\n");
             }
         }
+        
+        public void firstNode()
+        {
+            if (listEmpty())
+                Console.WriteLine("\nList is empty");
+            else 
+                Console.WriteLine("\nThe first record in the list is:\n\n" + LAST.next.rollNumber + " " + LAST.next.name);
+        }
 
         
 
