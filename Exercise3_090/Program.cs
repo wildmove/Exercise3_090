af7bea96e767b08b35934d9773c3b385953ef2b8
wildmove <muhammad.wildan,ft21@mail.umy.ac.id>
2022-11-25 16:05:22 +0700

    buat class Node

--- Exercise3_090/Program.cs
+++ Exercise3_090/Program.cs
@@ -6,5 +6,11 @@ using System.Threading.Tasks;
 
 namespace Exercise_Linked_List_A
 {
-    
+    class Node
+    {
+        /*creates Nodes for the circular nexted list*/
+        public int rollNumber;
+        public string name;
+        public Node next;
+    }
 }
