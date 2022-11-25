53ae7dcef7057d6187b9c2475e070413d6467fea
wildmove <muhammad.wildan,ft21@mail.umy.ac.id>
2022-11-25 16:15:11 +0700

    buat method traverse

--- Exercise3_090/Program.cs
+++ Exercise3_090/Program.cs
@@ -46,7 +46,24 @@ namespace Exercise_Linked_List_A
                 return false;
         }
 
+        public void traverse()
+        {
 
+            if (listEmpty())
+                Console.WriteLine("\nList is empty");
+            else
+            {
+                Console.WriteLine("\nRecords in the list are:\n");
+                Node currentNode;
+                currentNode = LAST.next;
+                while (currentNode != LAST)
+                {
+                    Console.Write(currentNode.rollNumber + "  " + currentNode.name + "\n");
+                    currentNode = currentNode.next;
+                }
+                Console.Write(LAST.rollNumber + " " + LAST.name + "\n");
+            }
+        }
 
         
 
