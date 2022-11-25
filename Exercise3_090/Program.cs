d43d394d94b3dfd277acb8c8a6647a03866f6947
wildmove <muhammad.wildan,ft21@mail.umy.ac.id>
2022-11-25 16:10:28 +0700

    buat method Search

--- Exercise3_090/Program.cs
+++ Exercise3_090/Program.cs
@@ -23,6 +23,20 @@ namespace Exercise_Linked_List_A
             LAST = null;
         }
 
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+        /*Searches for the specified node*/
+        {
+            for(previous = current = LAST.next; current != LAST; previous = current, current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                    return (true);/*returns true if the node is found*/
+            }
+            if (rollNo == LAST.rollNumber)/*If the node is present at the end*/
+                return true;
+            else
+                return (false);/*returns false if the node is not found*/
+        }
+
         
 
     }
